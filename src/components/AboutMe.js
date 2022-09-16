acfd987d8111bc474b8712e25fca263f83c97b47
Pushkar Jadhao <pushkar.jadhao@gmail.com>
2022-09-16 17:58:52 +0530

    added about me component

---
+++ src/components/AboutMe.js
@@ -0,0 +1,108 @@
+/** @format */
+
+import React from "react";
+import Anime, { anime } from "react-anime";
+import { Award, Folder, Users } from "react-feather";
+import aboutme from "../assests/aboutme.svg";
+
+const AboutMe = () => {
+	return (
+		<div
+			id="aboutme"
+			className="flex flex-row items-center justify-around mt-[2.5in] py-5"
+		>
+			{/* <div className="h-64 w-64 bg-[#555]"></div> */}
+			<div className="w-[35%] flex flex-col gap-5">
+				<h1 className="text-primaryTxt font-[kontanter] text-7xl text-center mb-10">
+					ABOUT ME
+				</h1>
+				<div className="flex justify-between mb-10">
+					<div className="w-[13rem] h-[13rem] bg-secondaryBg rounded-3xl flex flex-col items-center justify-between p-5">
+						<h3 className="text-primaryTxt">EXPERIENCE</h3>
+						<Anime
+							scale={{
+								value: 1.1,
+								easing: "linear",
+								duration: anime.random(500, 1000),
+							}}
+							loop={true}
+							direction={"alternate"}
+							delay={300}
+						>
+							<Award color="#2dc653" size={42} />
+						</Anime>
+						<h3 className="text-[#9ca3af]">5+ Years</h3>
+					</div>
+
+					<div className="w-[13rem] h-[13rem] bg-secondaryBg rounded-3xl flex flex-col items-center justify-between p-5">
+						<h3 className="text-primaryTxt">PROJECTS</h3>
+
+						<Anime
+							scale={{
+								value: 1.1,
+								easing: "linear",
+								duration: anime.random(500, 1000),
+							}}
+							loop={true}
+							direction={"alternate"}
+							delay={300}
+						>
+							<Folder color="#2dc653" size={42} />
+						</Anime>
+						<h3 className="text-[#9ca3af]">20+ Active</h3>
+					</div>
+					<div className="w-[13rem] h-[13rem] bg-secondaryBg rounded-3xl flex flex-col items-center justify-between p-5">
+						<h3 className="text-primaryTxt">CLIENTS</h3>
+						<Anime
+							scale={{
+								value: 1.1,
+								easing: "linear",
+								duration: anime.random(500, 1000),
+							}}
+							loop={true}
+							direction={"alternate"}
+							delay={300}
+						>
+							<Users color="#2dc653" size={42} />
+						</Anime>
+						<h3 className="text-[#9ca3af]">100+ Active</h3>
+					</div>
+				</div>
+				<p className="text-primaryTxt font-normal leading-7 tracking-wide text-[.9rem]">
+					Here's a gist, I'm{" "}
+					<em className="text-[#f77f00] font-medium">
+						a Programmer by Profession, a Hacker by Choice, and a Blogger by
+						Interest
+					</em>
+					. As a graduate, I hold a Bachelors Degree in Computer Science and
+					Engineering from Savitribai Phule Pune University (formerly University
+					of Pune).
+				</p>
+				<p className="text-primaryTxt font-normal leading-7 tracking-wide text-[.9rem]">
+					I've worked on several projects throughout years which includes Web
+					Development, Graphics Designing, Android Development along with
+					Android ROM Development as well.
+				</p>
+				<p className="text-primaryTxt font-normal leading-7 tracking-wide text-[.9rem]">
+					I believe in qualitative work rather than quantitative and believe
+					that my skills will support your brand and/or you to succeed in your
+					business and objectives.
+				</p>
+				<p className="text-primaryTxt font-normal leading-7 tracking-wide text-[.9rem]">
+					Feel free to get in touch.
+				</p>
+				<a
+					href="#contact"
+					className="text-primaryTxt border-2 border-[#f1faee] w-fit px-5 py-2 rounded-2xl transition ease-in-out duration-300 hover:bg-[#f1faee] hover:text-[#212121] mt-2"
+				>
+					Let's Talk
+				</a>
+			</div>
+			<div className="w-[550px]">
+				<img src={aboutme} alt="About Pushkar" />
+			</div>
+		</div>
+	);
+};
+
+export default AboutMe;
