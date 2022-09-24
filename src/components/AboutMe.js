870224861bea1ffc8e8ca3e9da27f804524c5a76
Pushkar Jadhao <pushkar.jadhao@gmail.com>
2022-09-24 10:56:08 +0530

    added responsiveness

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -9,16 +9,15 @@ const AboutMe = ({ refs }) => {
 	return (
 		<div
 			id="aboutme"
-			className="flex flex-row items-center justify-around mt-[2.5in] py-5 snap-center"
+			className="flex flex-col md:flex-row items-center justify-around mt-[2.5in] py-5 snap-center"
 			ref={refs}
 		>
-			{/* <div className="h-64 w-64 bg-[#555]"></div> */}
-			<div className="w-[35%] flex flex-col gap-5">
-				<h1 className="text-primaryTxt text-7xl font-medium text-center mb-10">
+			<div className="md:w-[35%] flex flex-col gap-5">
+				<h1 className="text-primaryTxt text-5xl	md:text-7xl font-medium text-center mb-10">
 					About Me
 				</h1>
-				<div className="flex justify-between mb-10">
-					<div className="w-[13rem] h-[13rem] bg-secondaryBg rounded-3xl flex flex-col items-center justify-between p-5">
+				<div className="flex flex-col md:flex-row md:justify-between justify-center items-center mb-10">
+					<div className="w-[13rem] h-[13rem] bg-secondaryBg rounded-3xl flex flex-col items-center justify-between p-5 m-2">
 						<h3 className="text-primaryTxt">EXPERIENCE</h3>
 						<Anime
 							scale={{
@@ -35,7 +34,7 @@ const AboutMe = ({ refs }) => {
 						<h3 className="text-[#9ca3af]">5+ Years</h3>
 					</div>
 
-					<div className="w-[13rem] h-[13rem] bg-secondaryBg rounded-3xl flex flex-col items-center justify-between p-5">
+					<div className="w-[13rem] h-[13rem] bg-secondaryBg rounded-3xl flex flex-col items-center justify-between p-5 m-2">
 						<h3 className="text-primaryTxt">PROJECTS</h3>
 
 						<Anime
@@ -52,7 +51,7 @@ const AboutMe = ({ refs }) => {
 						</Anime>
 						<h3 className="text-[#9ca3af]">20+ Active</h3>
 					</div>
-					<div className="w-[13rem] h-[13rem] bg-secondaryBg rounded-3xl flex flex-col items-center justify-between p-5">
+					<div className="w-[13rem] h-[13rem] bg-secondaryBg rounded-3xl flex flex-col items-center justify-between p-5 m-2">
 						<h3 className="text-primaryTxt">CLIENTS</h3>
 						<Anime
 							scale={{
@@ -69,37 +68,39 @@ const AboutMe = ({ refs }) => {
 						<h3 className="text-[#9ca3af]">100+ Active</h3>
 					</div>
 				</div>
-				<p className="text-primaryTxt font-normal leading-7 tracking-wide text-[.9rem]">
-					Here's a gist, I'm{" "}
-					<em className="text-[#f77f00] font-medium">
-						a Programmer by Profession, a Hacker by Choice, and a Blogger by
-						Interest
-					</em>
-					. As a graduate, I hold a Bachelors Degree in Computer Science and
-					Engineering from Savitribai Phule Pune University (formerly University
-					of Pune).
-				</p>
-				<p className="text-primaryTxt font-normal leading-7 tracking-wide text-[.9rem]">
-					I've worked on several projects throughout years which includes Web
-					Development, Graphics Designing, Android Development along with
-					Android ROM Development as well.
-				</p>
-				<p className="text-primaryTxt font-normal leading-7 tracking-wide text-[.9rem]">
-					I believe in qualitative work rather than quantitative and believe
-					that my skills will support your brand and/or you to succeed in your
-					business and objectives.
-				</p>
-				<p className="text-primaryTxt font-normal leading-7 tracking-wide text-[.9rem]">
-					Feel free to get in touch.
-				</p>
-				<a
-					href="#contact"
-					className="text-primaryTxt border-2 border-[#f1faee] w-fit px-5 py-2 rounded-2xl transition ease-in-out duration-300 hover:bg-[#f1faee] hover:text-[#212121] mt-2"
-				>
-					Let's Talk
-				</a>
+				<div className="flex flex-col gap-5 justify-center">
+					<p className="text-primaryTxt font-normal leading-7 tracking-wide text-[.9rem]">
+						Here's a gist, I'm{" "}
+						<em className="text-[#f77f00] font-medium">
+							a Programmer by Profession, a Hacker by Choice, and a Blogger by
+							Interest
+						</em>
+						. As a graduate, I hold a Bachelors Degree in Computer Science and
+						Engineering from Savitribai Phule Pune University (formerly
+						University of Pune).
+					</p>
+					<p className="text-primaryTxt font-normal leading-7 tracking-wide text-[.9rem]">
+						I've worked on several projects throughout years which includes Web
+						Development, Graphics Designing, Android Development along with
+						Android ROM Development as well.
+					</p>
+					<p className="text-primaryTxt font-normal leading-7 tracking-wide text-[.9rem]">
+						I believe in qualitative work rather than quantitative and believe
+						that my skills will support your brand and/or you to succeed in your
+						business and objectives.
+					</p>
+					<p className="text-primaryTxt font-normal leading-7 tracking-wide text-[.9rem]">
+						Feel free to get in touch.
+					</p>
+					<a
+						href="#contact"
+						className="text-primaryTxt border-2 border-[#f1faee] w-fit px-5 py-2 rounded-2xl transition ease-in-out duration-300 hover:bg-[#f1faee] hover:text-[#212121] mt-2"
+					>
+						Let's Talk
+					</a>
+				</div>
 			</div>
-			<div className="w-[500px]">
+			<div className="md:w-[500px] w-full">
 				<img src={aboutme} alt="About Pushkar" />
 			</div>
 		</div>
